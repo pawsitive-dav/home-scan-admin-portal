d34751815dcdea7b5c9bad08b434738d6a60dc71
NIPA-LT0053 <nipa-lt0053@NIPA-LT0053s-MacBook-Air.local>
2023-09-27 00:32:09 +0700

    first commit

---
+++ components/PageLoading.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="loading-body" :class="classList">
+    <div class="spinner-box">
+      <div class="circle-border">
+        <div class="circle-core"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      classList: [],
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.classList.push('stop-loading')
+    }, 2000)
+  },
+  methods: {
+    checkToken() {
+      //
+    },
+  },
+}
+</script>
+
+<style scoped>
+.loading-body {
+  position: fixed;
+  z-index: 99;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fbfbfb;
+  transition: all ease 0.3s;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+
+.spinner-box {
+  width: 300px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+}
+
+.circle-border {
+  width: 80px;
+  height: 80px;
+  padding: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background: rgb(63, 249, 220);
+  background: linear-gradient(
+    0deg,
+    rgba(63, 249, 220, 0.1) 33%,
+    rgb(41, 218, 191) 100%
+  );
+  animation: spin 0.8s linear 0s infinite;
+}
+
+.circle-core {
+  width: 100%;
+  height: 100%;
+  background-color: #fbfbfb;
+  border-radius: 50%;
+}
+
+.stop-loading {
+  opacity: 0;
+  z-index: 0;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  ssr: false,
+
+  head: {
+    titleTemplate: '%s - admin-portal',
+    title: 'admin-portal',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+  },
+
+  css: ['~/assets/main.css'],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/vuetify'],
+
+  modules: ['@nuxtjs/axios'],
+
+  axios: {
+    baseURL: '/',
+  },
+
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        light: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
