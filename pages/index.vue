538ea171eaf1cfd8eacb9e505b44b43c21d4fa0e
NIPA-LT0053 <nipa-lt0053@NIPA-LT0053s-MacBook-Air.local>
2023-11-24 23:41:06 +0700

    แก้ไข text หน้า Dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,11 @@
 <template>
-  <div>
-    <v-btn color="primary" @click="onPDFCreate()">PDF</v-btn>
-  </div>
+  <div>Developing</div>
 </template>
 
 <script>
-import '@/static/Sarabun-Regular-normal.js'
-import jsPDF from 'jspdf'
-
 export default {
-  data() {
-    return {
-      pdfOption: {
-        orientation: 'p',
-        format: 'a4',
-        unit: 'px',
-        lineHeight: 2,
-        putOnlyUsedFonts: true,
-      },
-    }
-  },
-  methods: {
-    onPDFCreate() {
-      // eslint-disable-next-line new-cap
-      const pafDoc = new jsPDF()
-      pafDoc.setFont('Sarabun-Regular', 'normal')
-      pafDoc.setTextColor('#265B7F')
-      pafDoc.text('สวัสดีภาษาไทย', 10, 10)
-      pafDoc.save('pdf-thai.pdf')
-    },
+  head: {
+    title: 'Dashboard',
   },
 }
 </script>
